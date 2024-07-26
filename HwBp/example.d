4ff10c66696d149d8174799ef68c3d907e704de9
dk0m <hookirp@gmail.com>
2024-07-26 13:13:53 -0700

    Update example.d

--- HwBp/example.d
+++ HwBp/example.d
@@ -5,6 +5,18 @@ import core.sys.windows.windows;
 
 import hwbp;
 
+// MessageBoxA Hook Example, Changing Title And Caption.
+void msgBoxAHook(PCONTEXT ctxRec) {
+
+    LPCSTR title = "Hooked!";
+    LPCSTR caption = "Get Hooked!";
+
+    SETPARAM_2(ctxRec, cast(ULONG_PTR)title);
+    SETPARAM_3(ctxRec, cast(ULONG_PTR)caption);
+
+    CONTINUE_EXECUTION(ctxRec);
+}
+
 void main() {
 
     initHwBpVars();
